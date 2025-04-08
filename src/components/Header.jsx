e6de495e04f843e3fa3003c52914311d8be900c5
robert thornton <robert.thornton92@gmail.com>
2025-04-08 14:26:51 +0300

    Created: header component holding title and reset button.

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+
+function Header({ reset }) {
+
+    return (
+        <header className="header">
+            <h1>Memory Game</h1>
+            <div className="navbar">
+                <button className="reset-button" onClick={reset}>Reset</button>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
